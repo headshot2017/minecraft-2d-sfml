637ba13dcbe63e4bb8c3af1d70e11e368add109f
Mauricio <juanpablobouzas@gmail.com>
2021-02-28 01:59:25 -0400

    remove config.lua

--- config.lua
+++
@@ -1,7 +0,0 @@
-compiler.driver = 'gcc'
-compiler.c_compiler = false
-compiler.cxx_compiler = false
-stackprotector.value = false
-minmacosxsdk.value = false
-macosxppc.value = false
-sfml.value = true
